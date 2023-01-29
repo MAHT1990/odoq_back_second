e1875fe9d5731f38bfb115a1f2fd65297e15d994
Marotik <kitoram3@gmail.com>
2023-01-30 01:48:56 +0900

    question serving 로직 보안.

--- api/question/services.py
+++ api/question/services.py
@@ -15,27 +15,45 @@ class GetQuestion:
 
         queryset_question = OdoqModels.Question.objects.all()
         # TODO: 없으면 None으로 잡히고, None의 attribute 접근에 대하여 AttributeError
-        list_question_current_and_next = [
-            min(
-                list(
-                    map(
-                        lambda q: (abs(round((now_utc - q.upload_datetime).total_seconds())), q),
-                        qs
-                    )
-                ),
-                key=lambda x: x[0]
-            ) for qs in [
-                queryset_question.filter(
-                    upload_datetime__lte=now_utc
-                ),
-                queryset_question.filter(
-                    upload_datetime__gte=now_utc
-                )
-            ]
-        ]
+        if len(queryset_question) > 0:
+            try:
+                list_question_current_and_next = [
+                    min(
+                        list(
+                            map(
+                                lambda q: (abs(round((now_utc - q.upload_datetime).total_seconds())), q),
+                                qs
+                            )
+                        ),
+                        key=lambda x: x[0]
+                    ) for qs in [
+                        queryset_question.filter(
+                            upload_datetime__lte=now_utc
+                        ),
+                        queryset_question.filter(
+                            upload_datetime__gte=now_utc
+                        )
+                    ]
+                ]
 
-        self.question = list_question_current_and_next[0][1]
-        self.second_remain = list_question_current_and_next[1][0]
+                self.question = list_question_current_and_next[0][1]
+                self.second_remain = list_question_current_and_next[1][0]
+            except ValueError as e:
+                print(e)
+                self.question = min(
+                    list(
+                        map(
+                            lambda q: (abs(round((now_utc - q.upload_datetime).total_seconds())), q),
+                            queryset_question.filter(
+                                upload_datetime__lte=now_utc
+                            )
+                        )
+                    ), key=lambda x: x[0]
+                )[1]
+                self.second_remain = None
+        else:
+            self.question, self.second_remain = None, None
+
 
     def make_data(self):
         self._get_question()
@@ -48,5 +66,6 @@ class GetQuestion:
             'solve_count': self.question.solve_count,
             'second_remain': self.second_remain,
         }
+        print(self.data)
         return self.data
 
