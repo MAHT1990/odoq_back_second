9bf5d36e3df3f06e0152d6b27bc7f1e19fc341e8
Marotik <kitoram3@gmail.com>
2023-01-30 01:49:14 +0900

    post 조회 로직 시작.

--- api/post/views.py
+++ api/post/views.py
@@ -1,9 +1,19 @@
-from django.http import JsonResponse
 from rest_framework.views import APIView
+from common._RES import makeResponse
+from . import services
 
 def index(request):
     return JsonResponse({
         'test': 'test',
     })
 
-
+class GetPost(APIView):
+    def get(self, request):
+        print('getPost get called')
+        result = services.GetPost(request).make_data()
+        response = makeResponse(
+            'status',
+            'message',
+            result,
+        )
+        return response
