f6f2f70de40ddd58de3022658908e19a1bfd9f81
Marotik <kitoram3@gmail.com>
2023-02-02 01:18:40 +0900

    댓글 Reponse 로직 추가.

--- api/post/services.py
+++ api/post/services.py
@@ -1,16 +1,31 @@
 import odoq_models.models as OdoqModels
+from . import serializers
 
 class GetPost:
     def __init__(self, request):
         self.request = request
 
-    def _get_post(self):
-        queryset_post = OdoqModels.Post.objects.all()
-        self.post = queryset_post
-    def make_data(self):
-        self._get_post()
         self.data = {
-
+            'posts': [],
         }
+
+    def _get_dict_posts(self):
+        queryset_post = OdoqModels.Post.objects.all()
+        # self.post = serializers.PostSerializer(data=queryset_post, many=True)
+        for post in queryset_post:
+            self.data['posts'].append({
+                'id': post.id,
+                'user': post.user.name,
+                'content': post.content,
+                'like_count': post.like_count,
+                'created_at': post.created_at,
+                'updated_at': post.updated_at,
+                'blind': post.blind,
+                'blind_text': post.blind_text,
+            })
+
+
+    def make_data(self):
+        self._get_dict_posts()
         print(self.data)
         return self.data
