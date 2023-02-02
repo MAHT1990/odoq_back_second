9d6a656c16214f31a0e51af29c1ca09498ea7955
Marotik <kitoram3@gmail.com>
2023-02-03 01:41:10 +0900

    댓글 Pagintaion 로직 추가.

--- api/post/services.py
+++ api/post/services.py
@@ -1,9 +1,11 @@
 import odoq_models.models as OdoqModels
-from . import serializers
+from django.core.paginator import Paginator
 
 class GetPost:
     def __init__(self, request):
         self.request = request
+        self.page_number = request.GET.get('pageNumber', 1)
+        self.page_size = request.GET.get('pageSize', 7)
 
         self.data = {
             'posts': [],
@@ -11,9 +13,9 @@ class GetPost:
 
     def _get_dict_posts(self):
         queryset_post = OdoqModels.Post.objects.all()
-        # self.post = serializers.PostSerializer(data=queryset_post, many=True)
+        list_temp_posts = []
         for post in queryset_post:
-            self.data['posts'].append({
+            list_temp_posts.append({
                 'id': post.id,
                 'user': post.user.name,
                 'content': post.content,
@@ -23,9 +25,11 @@ class GetPost:
                 'blind': post.blind,
                 'blind_text': post.blind_text,
             })
-
+        pagenation = Paginator(list_temp_posts, self.page_size)
+        list_result_posts = pagenation.page(self.page_number).object_list
+        self.data['posts'] = list_result_posts
 
     def make_data(self):
         self._get_dict_posts()
-        print(self.data)
+        # print(self.data)
         return self.data
