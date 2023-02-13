55579f6edf118e96a40abe01197b6afb63b77675
Marotik <kitoram3@gmail.com>
2023-02-14 00:32:52 +0900

    배포를 위한 개발환경 세팅
    .

--- odoq_backend_second/settings.py
+++ odoq_backend_second/settings.py
@@ -12,26 +12,31 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+with open(os.path.join(BASE_DIR, 'secrets.json'), 'rb') as secret_file:
+    secrets = json.load(secret_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7!p6h!i87kp1krj@ygeeg0@h8r$c!@)(xcj8r*odw4pb1qvg5'
+SECRET_KEY = secrets['SECRET_KEY']
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = (secrets['DEBUG'] == 'True')
+
 ALIGO = {
     'API_KEY': 'dtwkui1b8e5yux73rl5e5c3g666yt861',
     'USER_ID': 'aloe89',
     'SENDER': '01077650903',
 }
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # CORS 관련
 CORS_ORIGIN_WHITELIST = [
