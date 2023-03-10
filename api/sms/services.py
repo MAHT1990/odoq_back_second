a7727e989ee05347205dfe102fd26207876f6f63
Marotik <kitoram3@gmail.com>
2023-03-11 01:37:22 +0900

    출제자 문자발송
    .

--- api/sms/services.py
+++ api/sms/services.py
@@ -9,15 +9,21 @@ class SendAuthorSMS():
     def __call__(self):
         if self.request_data.is_valid():
             # target_phone = self.request_data.data['phone']
-            target_phone = '01092445161'
+            target_phone_query_set = OdoqModels.User.objects.filter(grade=1)
+            target_phone_list = [user.phone for user in target_phone_query_set]
+            print('target_phone_list in SendAuthorSMS Service: ', target_phone_list)
             content = f"제출 답안수는 {self.request_data.data['answerCount']} 입니다."
             print(content)
 
-            result = OdoqModels.SmsHistory.send_message(send_to=target_phone, is_auth=False, content=content)
+            for target_phone in target_phone_list:
+                result = OdoqModels.SmsHistory.send_message(send_to=target_phone, is_auth=False, content=content)
+                print(result)
+            # result = OdoqModels.SmsHistory.send_message(send_to=target_phone, is_auth=False, content=content)
 
-            if result:
-                return {'success': True, 'message': None}
-            else:
-                return {'success': False, 'message': '문자 발송요청에 실패하였습니다.'}
+            # if result:
+            #     return {'success': True, 'message': None}
+            # else:
+            #     return {'success': False, 'message': '문자 발송요청에 실패하였습니다.'}
+            return {'success': True, 'message': None}
         else:
             return {'success': False, 'message': common.serializer_error_message(self.request_data.errors)}
