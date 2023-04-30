531820b7499645c4dc4d0ec2e14ec1c46a960641
Marotik <kitoram3@gmail.com>
2023-05-01 02:18:39 +0900

    기능부착 시작
    .

--- odoq_models/models.py
+++ odoq_models/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class User(models.Model):
     email = models.EmailField(unique=True)
@@ -16,14 +17,17 @@ class User(models.Model):
     accept_sms = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
+
 # SMS 인증 관련.
 def SMS_HISTORY_AUTH_EXPIRE():
     return timezone.now() + timezone.timedelta(minutes=3)
+
 
 class SmsHistoryAuth(models.Model):
     code = models.CharField(max_length=255)
     is_auth = models.BooleanField(default=False)
     expired_at = models.DateTimeField()
+
 
 class SmsHistory(models.Model):
     sms_type = models.CharField(max_length=255)
@@ -38,20 +42,20 @@ class SmsHistory(models.Model):
 
     @staticmethod
     def send_message(send_to, is_auth=False, content=''):
-        '''
+        """
         문자 발송용 static method.
         - send_to : 수신자 전화번호
         - 일반 문자: is_auth = False / content = 입력받음.
         - 인증용 문자: is_auth = True / content = 생성로직을 따름.
-        '''
+        """
         import requests
-        #문자 발송용 API 관련
+        # 문자 발송용 API 관련
 
         url = "https://apis.aligo.in/send/"
         content = content
         result = False
 
-        if(is_auth):
+        if is_auth:
             import random
             code = '{0:06d}'.format(random.randrange(999999))
             content = f'인증번호 [{code}]를 입력창에 3분이내로 입력해주세요.'
@@ -85,21 +89,21 @@ class SmsHistory(models.Model):
     
     @staticmethod
     def verify_code(send_to, code):
-        '''
+        """
         * 해당번호(send_to)로 보낸 마지막 SmsHistory를 찾는다.
         * 마지막 SmsHistory 기록의 auth 필드가 존재하고, 아직 인증이 완료안됐으면
           만료일시와 현재 시각을 비교하고
         * 받은 code값을 비교하여, 인증을 완료한 후 저장
         * return : 0 if 성공 else -1
-        '''
+        """
         last_auth = SmsHistory.objects.filter(
             sms_type='SMS', 
             send_to=send_to
             ).order_by('-sent_at').first()
-        if(last_auth):
-            if (last_auth.auth.is_auth == False):
-                if(last_auth.auth.expired_at > timezone.now()):
-                    if(last_auth.auth.code == code.strip()):
+        if last_auth:
+            if not last_auth.auth.is_auth:
+                if last_auth.auth.expired_at > timezone.now():
+                    if last_auth.auth.code == code.strip():
                         last_auth.auth.is_auth = True
                         last_auth.save()
                         return 0
@@ -117,6 +121,7 @@ class SmsHistory(models.Model):
             pass
         return -1
 
+
 class Notice(models.Model):
     season = models.CharField(max_length=255)
     img = models.ImageField(null=True)
@@ -129,6 +134,7 @@ class Notice(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+
 
 class Question(models.Model):
     code = models.CharField(max_length=255)
@@ -146,6 +152,7 @@ class Question(models.Model):
     def __str__(self):
         return self.code
 
+
 class AnswerHistory(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -156,14 +163,18 @@ class AnswerHistory(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+
     def __str__(self):
         return self.answer
+
+
 class Post(models.Model):
     # Question과 Comment는 1 : n 의 관계이다.
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     img = models.ImageField(null=True)
-    title = models.CharField(max_length=255, default='제목 이전 포스트들입니다.')
+    title = models.CharField(max_length=255, default='제목 만들어지기전 포스트들입니다.')
     content = models.TextField()
+    read_count = models.PositiveIntegerField(default=0)
     like_count = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -175,6 +186,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+class Solution(Post):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255)
+    isSolved = models.BooleanField(default=False)
+
 
 class Admin(models.Model):
     email = models.EmailField()
