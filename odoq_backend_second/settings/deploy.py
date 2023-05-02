31758d2217b3db5aeb60163b31bbaa1ce9ad8c40
Marotik <kitoram3@gmail.com>
2023-05-02 23:49:22 +0900

    DB분리
    .

--- odoq_backend_second/settings/deploy.py
+++ odoq_backend_second/settings/deploy.py
@@ -1,26 +1,12 @@
 from .base import *
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'awslearner',
-#         'USER': 'awslearner',
-#         'PASSWORD': 'v3nop101slk#',
-#         'HOST': 'awslearner.clbg2pjhuyja.ap-northeast-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'odoqtestrds2',
-        'USER': 'odoqtestrds2',
+        'NAME': 'awslearner',
+        'USER': 'awslearner',
         'PASSWORD': 'v3nop101slk#',
-        'HOST': 'odoqtestrds2.clbg2pjhuyja.ap-northeast-2.rds.amazonaws.com',
+        'HOST': 'awslearner.clbg2pjhuyja.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
