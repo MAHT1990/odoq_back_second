b6332946dfa420f2dcd7db3f610939961df4f524
Marotik <kitoram3@gmail.com>
2023-05-03 00:52:51 +0900

    TEST서버 DB이전
    .

--- odoq_backend_second/settings/deploy.py
+++ odoq_backend_second/settings/deploy.py
@@ -1,12 +1,13 @@
 from .base import *
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'awslearner',
-        'USER': 'awslearner',
+        'NAME': 'odoqrdstest',
+        'USER': 'odoqrdstest',
         'PASSWORD': 'v3nop101slk#',
-        'HOST': 'awslearner.clbg2pjhuyja.ap-northeast-2.rds.amazonaws.com',
+        'HOST': 'odoqrdstest.clbg2pjhuyja.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
