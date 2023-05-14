91236e786b64a70ad4ec29ad5ec33d5d60f9d8cb
Marotik <kitoram3@gmail.com>
2023-05-15 02:25:08 +0900

    deploy DB settings
    .

--- odoq_backend_second/settings/deploy.py
+++ odoq_backend_second/settings/deploy.py
@@ -4,10 +4,10 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'odoqrdstest',
-        'USER': 'odoqrdstest',
+        'NAME': 'odoq-rds',
+        'USER': 'odoq_admin',
         'PASSWORD': 'v3nop101slk#',
-        'HOST': 'odoqrdstest.clbg2pjhuyja.ap-northeast-2.rds.amazonaws.com',
+        'HOST': 'odoq-rds.cihq6njmtbfz.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
