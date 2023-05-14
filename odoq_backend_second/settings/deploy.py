e9ac86deba571c898039247f3d1dce848d7ec13e
Marotik <kitoram3@gmail.com>
2023-05-15 02:29:38 +0900

    odoq rds setting
    .

--- odoq_backend_second/settings/deploy.py
+++ odoq_backend_second/settings/deploy.py
@@ -4,7 +4,7 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'odoq-rds',
+        'NAME': 'odoq',
         'USER': 'odoq_admin',
         'PASSWORD': 'v3nop101slk#',
         'HOST': 'odoq-rds.cihq6njmtbfz.ap-northeast-2.rds.amazonaws.com',
