2d4781814d688b37f153a5683c7b93b45f448b0c
Marotik <kitoram3@gmail.com>
2023-09-03 23:04:47 +0900

    [FIX]닉네임변경BE로직 수정
    .

--- api/user/views.py
+++ api/user/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from middleware.CSRF import csrf_decorator
 from common._RES import make_response
 from common._ENCRYPT import ENCRYPT
+from common._JWT import JWT
 import odoq_models.models as OdoqModels
+from .login import dictionaries
 
 USER_MODEL = OdoqModels.User
 
@@ -26,4 +28,11 @@ class EditUserView(APIView):
 
         user.name = request.data['userName']
         user.save()
-        return make_response('success', '닉네임 수정 완료')
+        token = JWT.sign(user.id, user.grade, user.name)
+        data = dictionaries.GetDataTable(token).make_data()
+        data['userName'] = user.name
+        return make_response(
+            'success',
+            '닉네임 수정 완료',
+            data
+        )
